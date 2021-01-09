93edd35d3fe0b25fe27435007f6bfe2efca81a69
twango <jamesding365@gmail.com>
2021-01-09 01:30:56 -0800

    start

--- TicTacToe Game/main.py
+++ TicTacToe Game/main.py
@@ -1,14 +1,5 @@
-import pygame
 
-screen_resolution = [600, 600]
-screen_name = "Tic Tac Toe"
-fps = 60
-
-pygame.init()
-game_display = pygame.display.set_mode(screen_resolution)
-pygame.display.set_caption(screen_name)
-
-#Import assets (none for now)
+board_data = []
 
 class Board():
     board_values = []
@@ -17,5 +8,3 @@ class Board():
         # aston you do it
         pass
 
-def start_screen():
-    pass
